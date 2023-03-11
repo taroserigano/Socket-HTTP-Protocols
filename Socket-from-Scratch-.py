9b9f76fc212e9bc37c937ff26b21eccdf668a51d
Taro Serigano <taroserigano@gmail.com>
2023-03-11 16:19:00 -0600

    Update Socket-from-Scratch-.py

--- Socket-from-Scratch-.py
+++ Socket-from-Scratch-.py
@@ -107,6 +107,8 @@ def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read / accept 
     print(f"Accepted connection from {addr}")
     conn.setblocking(False)    # set as non-blocking mode 
+    
+    # SimpleNameSpace is an object to hold the data with the socket 
     data = types.SimpleNamespace(addr=addr, inbound=b"", outbound=b"")
     # switch if event is read or write when connection with
     # client is established 
@@ -139,14 +141,122 @@ def service_connection(key, mask):
             data.outb = data.outb[sent:] # remove the sent part from the entire data.outb 
 
 
+----------------------------------------------------------------------
+
+
+Multi-Connection Client 
+
+
+# multiconn-client.py
+
+import sys
+import socket
+import selectors
+import types
+
+sel = selectors.DefaultSelector()
+messages = [b"Message 1 from client.", b"Message 2 from client."]
+
+
+# num_conns is the number of connections to create to the server 
+# each socket is set to non-blocking mode 
+def start_connections(host, port, num_conns):
+    
+    server_addr = (host, port)
+    for i in range(0, num_conns):
+        connid = i + 1
+        print(f"Starting connection {connid} to {server_addr}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        
+        # instead of connect() 
+        # this throws error indicator, errno.EINPROGRESS
+        # instead of an exception that can cause interference 
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        
+        # SimpleNameSpace is an object to hold the data with the socket 
+        data = types.SimpleNamespace(
+            connid=connid,
+            msg_total=sum(len(m) for m in messages), # total number of bytes in the messages 
+            recv_total=0, 
+            messages=messages.copy(),
+            outb=b"",
+        )
+        sel.register(sock, events, data=data)
+
+
+# CHANGE 
+CHNGE
+
+ def service_connection(key, mask):
+     sock = key.fileobj
+     data = key.data
+     if mask & selectors.EVENT_READ:
+         recv_data = sock.recv(1024)  # Should be ready to read
+         if recv_data:
+-            data.outb += recv_data
++            print(f"Received {recv_data!r} from connection {data.connid}")
+            # count the total number of bytes coming from the server 
++            data.recv_total += len(recv_data)
+
+-        else:
+-            print(f"Closing connection {data.connid}")
++        if not recv_data or data.recv_total == data.msg_total:
++            print(f"Closing connection {data.connid}")
+             sel.unregister(sock)
+             sock.close()
+     if mask & selectors.EVENT_WRITE:
++        if not data.outb and data.messages:
++            data.outb = data.messages.pop(0)
+         if data.outb:
+-            print(f"Echoing {data.outb!r} to {data.addr}")
++            print(f"Sending {data.outb!r} to connection {data.connid}")
+             sent = sock.send(data.outb)  # Should be ready to write
+             data.outb = data.outb[sent:]
 
 
 
 
 
+Running the Multi-Connection Client and Server
+ 
 
+Running the Multi-Connection Client and Server
+Now it’s time to run multiconn-server.py and multiconn-client.py. They both use command-line arguments. You can run them without arguments to see the options.
 
+For the server, pass host and port numbers:
 
+$ python multiconn-server.py
+Usage: multiconn-server.py <host> <port>
+For the client, also pass the number of connections to create to the server, num_connections:
+
+$ python multiconn-client.py
+Usage: multiconn-client.py <host> <port> <num_connections>
+Below is the server output when listening on the loopback interface on port 65432:
+
+$ python multiconn-server.py 127.0.0.1 65432
+Listening on ('127.0.0.1', 65432)
+Accepted connection from ('127.0.0.1', 61354)
+Accepted connection from ('127.0.0.1', 61355)
+Echoing b'Message 1 from client.Message 2 from client.' to ('127.0.0.1', 61354)
+Echoing b'Message 1 from client.Message 2 from client.' to ('127.0.0.1', 61355)
+Closing connection to ('127.0.0.1', 61354)
+Closing connection to ('127.0.0.1', 61355)
+Below is the client output when it creates two connections to the server above:
+
+$ python multiconn-client.py 127.0.0.1 65432 2
+Starting connection 1 to ('127.0.0.1', 65432)
+Starting connection 2 to ('127.0.0.1', 65432)
+Sending b'Message 1 from client.' to connection 1
+Sending b'Message 2 from client.' to connection 1
+Sending b'Message 1 from client.' to connection 2
+Sending b'Message 2 from client.' to connection 2
+Received b'Message 1 from client.Message 2 from client.' from connection 1
+Closing connection 1
+Received b'Message 1 from client.Message 2 from client.' from connection 2
+Closing connection 2 
+ 
 
 
 
